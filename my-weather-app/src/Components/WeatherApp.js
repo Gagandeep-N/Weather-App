bdb6e680e37bc0f80b4d0f9e7b0211128ac95735
Gagandeep-N <gagandeep.n.2001@gmail.com>
2024-02-29 18:29:01 +0530

    Created input field

--- my-weather-app/src/Components/WeatherApp.js
+++ my-weather-app/src/Components/WeatherApp.js
@@ -1,22 +1,44 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'; 
-
+import '../Styles/WeatherApp.css';
 
 const WeatherApp = () => {
     const  [weather, setWeather] = useState(null);
+    const [city, setCity] = useState('');
     const apiKey = process.env.REACT_APP_API_KEY;
     const apiUrl = process.env.REACT_APP_API_URL;
-    const city = "London";
+
+    const setCityHandler = (event) => {
+       const newCity = event.target.value;
+       setCity(newCity)
+    }
+
+    const clearInput =  () => {
+        setCity("");
+    };
 
     useEffect(()=>{
         axios.get(`${apiUrl}?q=${city}&appid=${apiKey}`)
         .then((response) => {
-            console.log(response);
             console.log(response.data);
             setWeather(response.data);
         })
         .catch((error)=>console.log(error));
-    }, [apiKey,city]);
+    }, [apiKey,apiUrl,city]);
+
+    return (
+        <div className="container">
+        <div className="input-clear">
+            <input type="text" placeholder="Enter the city name" value={city} onChange={setCityHandler} className="city-input"/>
+            {city && (
+        <button className="clear-button" onClick={clearInput}>
+          x
+        </button>
+        
+      )}
+      </div>
+        </div>
+    );
 }
 
 export default WeatherApp;
