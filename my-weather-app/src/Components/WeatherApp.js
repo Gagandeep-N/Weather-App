6f830585870252a97ed99a4784fdd6eb3b9e025e
Gagan-ELS <g.nandakishore@elsevier.com>
2024-03-07 15:23:19 +0530

    Created UI for the app

--- my-weather-app/src/Components/WeatherApp.js
+++ my-weather-app/src/Components/WeatherApp.js
@@ -1,53 +1,65 @@
-import { useEffect, useState } from "react";
-import axios from 'axios'; 
+import { useState } from "react";
+import axios from 'axios';
 import '../Styles/WeatherApp.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudSun } from '@fortawesome/free-solid-svg-icons';
+
 
 const WeatherApp = () => {
-    const  [weather, setWeather] = useState(null);
+    const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('');
+    const [showWeather, setShowWeather] = useState(false);
     const apiKey = process.env.REACT_APP_API_KEY;
     const apiUrl = process.env.REACT_APP_API_URL;
 
     const setCityHandler = (event) => {
-       const newCity = event.target.value;
-       setCity(newCity)
+        const newCity = event.target.value;
+        setCity(newCity)
     }
 
-    const clearInput =  () => {
-        setCity("");
-    };
-
-    useEffect(()=>{
+    const submitHandler = () => {
         axios.get(`${apiUrl}?q=${city}&appid=${apiKey}`)
-        .then((response) => {
-            console.log(response.data);
-            setWeather(response.data);
-        })
-        .catch((error)=>console.log(error));
-    }, [apiKey,apiUrl,city]);
+            .then((response) => {
+                console.log(response.data);
+                setWeather(response.data);
+                setCity("");
+                setShowWeather(true)
+            })
+            .catch((error) => console.log(error));
+            
+    }
+
+    function kelvinToCelsius(kelvin) {
+        return kelvin - 273.15;
+    }
 
     return (
         <div className="container">
-        <div className="input-clear">
-            <input type="text" placeholder="Enter the city name" value={city} onChange={setCityHandler} className="city-input"/>
-            {city && (
-        <button className="clear-button" onClick={clearInput}>
-          x
-        </button>
-      )}
-      </div>
-      <div  className="weather-display">
-      {weather && (
-                <div>
-                    {/* <h2>Weather for {city}</h2> */}
-                    
-                 { console.log("weather",weather.list[0].weather[0].description
-)}
-                   
-                </div>
-            )}
+            <div className="container-top">
 
-      </div>
+            <h1 className="app-header">Weather</h1>
+            <div className="input-clear">
+                <input type="text" placeholder="Enter the city name" value={city} onChange={setCityHandler} className="city-input" />
+                
+            </div>
+            <div>
+                <button className="submit-btn" onClick={submitHandler}>Search</button>
+            </div>
+            </div>
+            {showWeather && 
+            <div className="weather-display">
+                {weather && (
+                    <div className="current-temp">
+                        <div className="icon"><FontAwesomeIcon icon={faCloudSun} className="icon-svg"/></div>
+                        <div className="temp-info">
+                        <p className="city">{weather.city.name}</p>
+                        <p className="celsius">{kelvinToCelsius(weather.list[0].main.temp).toFixed(2)}°C</p>
+                        <p className="info">{weather.list[0].weather[0].main}</p>
+                        </div>
+                    </div>
+                )}
+
+            </div>}
         </div>
     );
 }
